4ae1ca5f64ef546915bfc004aabe24756fe280ad
diego-arapovic <diego.arapovic@gmail.com>
2024-08-23 01:18:00 +0200

    added slurm script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=360:00:00
+#SBATCH --mem-per-cpu=10240
+#SBATCH --mail-type=END
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate forl_env
+
+python -u main.py
